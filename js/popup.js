2b8be5cc5c5178d174d146dd3da3bb4a6ff3769b
2jun0 <soo28819@naver.com>
2021-10-05 23:45:53 +0900

    Update 0.9.2

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,3 @@
-// toggle mute ad
 const ccStatusExmaple = document.getElementById('cc-status-example');
 const colorPicker1 = document.getElementById('color-picker1');
 const colorPicker2 = document.getElementById('color-picker2');
@@ -6,21 +5,57 @@ const colorPickerWapper1 = document.getElementById('color-picker1-wapper');
 const colorPickerWapper2 = document.getElementById('color-picker2-wapper');
 const langPicker = document.getElementById('lang-picker');
 
+function setLanguage(lang) {
+  langPicker.value = lang;
+  ccStatusExmaple.textContent = langPicker.value.toUpperCase()+' CC'
+  // Save data
+  chrome.storage.sync.set({ 'YT-SUBTITLE-FILTER_lang': lang }, () => {});
+}
+
+function setColor1(color1) {
+  colorPicker1.value = color1;
+  colorPickerWapper1.style.background = color1;
+  ccStatusExmaple.style.background = color1;
+  // Save data
+  chrome.storage.sync.set({ 'YT-SUBTITLE-FILTER_color1': color1 }, () => {});
+}
+
+function setColor2(color2) {
+  colorPicker2.value = color2;
+  colorPickerWapper2.style.background = color2;
+  ccStatusExmaple.style.color = color2;
+  // Save data
+  chrome.storage.sync.set({ 'YT-SUBTITLE-FILTER_color2': color2 }, () => {});
+}
+
 colorPicker1.onchange = () => { 
-  colorPickerWapper1.style.background = colorPicker1.value;
-  ccStatusExmaple.style.background = colorPicker1.value;
+  setColor1(colorPicker1.value);
 }
 colorPicker2.onchange = () => { 
-  colorPickerWapper2.style.background = colorPicker2.value;
-  ccStatusExmaple.style.color = colorPicker2.value;
+  setColor2(colorPicker2.value);
+}
+langPicker.onchange = () => {
+  setLanguage(langPicker.value);
 }
 
-colorPicker1.value = '#008000';
-colorPicker1.onchange();
-
-colorPicker2.value = '#ffffff';
-colorPicker2.onchange();
-
-langPicker.appendChild(
+(async () => {
+  // dynamic import
+  const src = chrome.runtime.getURL('js/lang.js');
+  var langs = (await import(src)).langs;
   
-)
+  // add langauge options
+  langs.forEach(lang => {
+    var option = document.createElement("option");
+    option.value = lang.code;
+    option.textContent = lang.displayName;
+    langPicker.appendChild(option);
+  });
+
+  // Load data
+  chrome.storage.sync.get(['YT-SUBTITLE-FILTER_lang', 'YT-SUBTITLE-FILTER_color1', 'YT-SUBTITLE-FILTER_color2'], (items) => {
+    console.log(items)
+    setLanguage(items['YT-SUBTITLE-FILTER_lang'] || 'en');
+    setColor1(items['YT-SUBTITLE-FILTER_color1'] || '#008000');
+    setColor2(items['YT-SUBTITLE-FILTER_color2'] || '#ffffff');
+  });
+})();
