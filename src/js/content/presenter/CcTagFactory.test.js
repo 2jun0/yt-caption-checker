76e8dd6c73ecc626867105dc9f7c7c5228e3b0a0
2jun0 <soo28819@gmail.com>
2023-02-12 18:10:53 +0900

    test: CcTagFactory

---
+++ src/js/content/presenter/CcTagFactory.test.js
@@ -0,0 +1,24 @@
+import { CcTagFactory } from './CcTagFactory.js'
+import { jest } from '@jest/globals'
+
+describe('CcTagFactory', () => {
+  /** @type {Document} */
+  let document
+  /** @type {CcTagFactory} */
+  let ccTagFactory
+
+  beforeEach(() => {
+    document = {
+      createElement: jest.fn().mockImplementation(tag => ({
+        appendChild: jest.fn(),
+        style: {}
+      }))
+    }
+    ccTagFactory = CcTagFactory(document)
+  })
+
+  it('should create a CcTagView', () => {
+    const ccTagView = ccTagFactory.createCcTagView('#00000000', '#000000', '1.2rem', 'en')
+    expect(ccTagView).toBeDefined()
+  })
+})
