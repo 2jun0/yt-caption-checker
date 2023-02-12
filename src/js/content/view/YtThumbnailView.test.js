445a9916031006d6a980327ee202497e203654fa
2jun0 <soo28819@gmail.com>
2023-02-13 00:15:05 +0900

    test: YtThumbnailView

---
+++ src/js/content/view/YtThumbnailView.test.js
@@ -0,0 +1,89 @@
+import { YtThumbnailView } from './YtThumbnailView.js'
+import { jest } from '@jest/globals'
+
+describe('YtThumbnailView', () => {
+
+  describe('preconditions', () => {
+    it('throw error if thumbnail element is not a tag', () => {
+      const nonThumbnailEl = {
+        tagName: 'nonA',
+        id: 'thumbnail',
+        href: 'https://www.youtube.com/watch?v=123456'
+      }
+      expect(YtThumbnailView(nonThumbnailEl)).toThrowError()
+    })
+
+    it('throw error if thumbnail element doesn\' have thumbnail id', () => {
+      const nonThumbnailEl = {
+        tagName: 'A',
+        id: 'non-thumbnail',
+        href: 'https://www.youtube.com/watch?v=123456',
+      }
+      expect(YtThumbnailView(nonThumbnailEl)).toThrowError()
+    })
+
+    it('throw error if thumbnail element doesn\' have href', () => {
+      const nonThumbnailEl = {
+        tagName: 'A',
+        id: 'thumbnail'
+      }
+      expect(YtThumbnailView(nonThumbnailEl)).toThrowError()
+    })
+  })
+
+  it('should return the video url', () => {
+    const thumbnailEl = {
+      href: 'https://www.youtube.com/watch?v=123456',
+    }
+    const ytThumbnailView = YtThumbnailView(thumbnailEl)
+
+    expect(ytThumbnailView.getVideoUrl()).toBe('https://www.youtube.com/watch?v=123456')
+  })
+
+  it('should insert cc tag to overlays', async () => {
+    const overlays = {
+      childElementCount: 1,
+      querySelector: () => {},
+      insertBefore: jest.fn()
+    }
+    const thumbnailEl = {
+      querySelector: () => overlays
+    }
+    const ccTagView = {
+      ccTagElement: () => {}
+    }
+    const ytThumbnailView = YtThumbnailView(thumbnailEl)
+
+    await ytThumbnailView.insertCcTag(ccTagView)
+    expect(overlays.insertBefore).toBeCalled()
+  })
+
+  it('should return true if cc tag exists', async () => {
+    const overlays = {
+      childElementCount: 1,
+      querySelector: () => 'el',
+      insertBefore: jest.fn()
+    }
+    const thumbnailEl = {
+      querySelector: () => overlays
+    }
+    const ytThumbnailView = YtThumbnailView(thumbnailEl)
+
+    expect(await ytThumbnailView.hasCcTag()).toBe(true)
+  })
+
+  it('should return false if cc tag does not exist', async () => {
+    const overlays = {
+      childElementCount: 1,
+      querySelector: () => null,
+      insertBefore: jest.fn()
+    }
+    const thumbnailEl = {
+      querySelector: () => overlays
+    }
+    const ytThumbnailView = YtThumbnailView(thumbnailEl)
+
+    expect(await ytThumbnailView.hasCcTag()).toBe(false)
+  })
+})
+
